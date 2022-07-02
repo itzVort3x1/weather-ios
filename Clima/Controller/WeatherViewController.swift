659ebe424cc9255979e079c481ca23018d0fbaeb
itzVort3x1 <saikaustubh10@gmail.com>
2022-07-02 08:04:08 +0530

    added user input interaction

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,17 +1,48 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
+    
     @IBOutlet weak var temperatureLabel: UILabel!
+    
     @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // self refers to the current view controller
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if(textField.text != ""){
+            return true
+        }else {
+            textField.placeholder = "Please Enter a City"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // use searchTextField.text to get the weather for that city.
+        searchTextField.text = ""
+        searchTextField.placeholder = "Search"
+    }
+    
 }
 
