40656f13f6c689f5ff525c46f2bd8fccfc289e07
itzVort3x1 <saikaustubh10@gmail.com>
2022-07-06 16:48:17 +0530

    added error handling for API requests

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -14,10 +14,23 @@ struct WeatherManager {
             //2. Create a URLSession
             let session = URLSession(configuration: .default)
             //3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
             //4. Start the task
             task.resume()
         }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+        
     }
     
 }
