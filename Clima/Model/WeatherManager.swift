97d2f497286e04dd29d03b272c8d3f8f2b836275
itzVort3x1 <saikaustubh10@gmail.com>
2022-07-28 09:40:19 +0530

    added weather data

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -22,11 +22,21 @@ struct WeatherManager {
                 
                 if let safeData = data {
                     let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString)
                 }
             }
             //4. Start the task
             task.resume()
         }
     }
+    
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+        }catch {
+            print(error)
+        }
+    }
+    
 }
